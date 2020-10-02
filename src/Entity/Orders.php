aacc2051657b50b9a02678fb12912e85ce366ec0
Norbert <64070603+PHPHitman@users.noreply.github.com>
2020-10-02 17:25:21 +0200

    Add registration and authentication

---
+++ src/Entity/Orders.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrdersRepository::class)
+ */
+class Orders
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Food;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Quantity;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Company;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Cost;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Surname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFood(): ?string
+    {
+        return $this->Food;
+    }
+
+    public function setFood(string $Food): self
+    {
+        $this->Food = $Food;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->Quantity;
+    }
+
+    public function setQuantity(int $Quantity): self
+    {
+        $this->Quantity = $Quantity;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->Company;
+    }
+
+    public function setCompany(string $Company): self
+    {
+        $this->Company = $Company;
+
+        return $this;
+    }
+
+    public function getCost(): ?int
+    {
+        return $this->Cost;
+    }
+
+    public function setCost(int $Cost): self
+    {
+        $this->Cost = $Cost;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->Surname;
+    }
+
+    public function setSurname(string $Surname): self
+    {
+        $this->Surname = $Surname;
+
+        return $this;
+    }
+}
